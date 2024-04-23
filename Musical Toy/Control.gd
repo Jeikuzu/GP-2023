5d89498a3413152286f4a7c08600cf91c747d282
Jeikuzu <duffyjake261@gmail.com>
2024-04-23 15:37:20 +0100

    E string
    e string fret 1-12

--- Musical Toy/Control.gd
+++ Musical Toy/Control.gd
@@ -8,3 +8,30 @@ func _on_button_2_pressed():
 	
 func _on_button_3_pressed():
 	$AudioStreamPlayer3.play()
+	
+func _on_button_4_pressed():
+	$AudioStreamPlayer4.play()
+	
+func _on_button_5_pressed():
+	$AudioStreamPlayer5.play()
+	
+func _on_button_6_pressed():
+	$AudioStreamPlayer6.play()
+	
+func _on_button_7_pressed():
+	$AudioStreamPlayer7.play()
+	
+func _on_button_8_pressed():
+	$AudioStreamPlayer8.play()
+	
+func _on_button_9_pressed():
+	$AudioStreamPlayer9.play()
+	
+func _on_button_10_pressed():
+	$AudioStreamPlayer10.play()
+	
+func _on_button_11_pressed():
+	$AudioStreamPlayer11.play()
+	
+func _on_button_12_pressed():
+	$AudioStreamPlayer12.play()
